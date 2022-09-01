c0ea348247c01e7ccfc8232b2e0f514d9a662208
shaunbenny24 <shaunchukkiriyan@gmail.com>
2022-09-01 17:00:31 +0530

    added

---
+++ x.js
@@ -0,0 +1,22 @@
+let menu=document.getElementById('shaun')
+
+menu.addEventListener("click",function(){
+    console.log('button is clicked')
+
+    document.getElementById('mynav').style.width="100%"
+
+    
+
+
+})
+let clos=document.getElementById('close')
+
+clos.addEventListener("click",function(){
+    console.log('button is clicked')
+
+    document.getElementById('mynav').style.width="0%"
+
+    
+
+
+})
